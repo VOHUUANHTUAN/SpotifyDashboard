4982872b4b3c8fd5be252655a2b48386ff4809f5
VOHUUANHTUAN <chienvuive19052001@gmail.com>
2024-07-19 22:25:53 +0700

    final

--- script_20120477.js
+++ script_20120477.js
@@ -1,4 +1,4 @@
-d3.csv("spotify-2023.csv").then(function(data) {
+d3.csv("spotify-2023.csv").then(function (data) {
     // Filter out rows with empty key values
     data = data.filter(d => d.key !== "");
 
@@ -9,82 +9,82 @@ d3.csv("spotify-2023.csv").then(function(data) {
     songCounts.sort((a, b) => b[1] - a[1]);
 
     // Dimensions
-    const margin = {  top: 10, right: 20, bottom: 110, left: 40 },
-          width = 700 - margin.left - margin.right,
-          height = 400 - margin.top - margin.bottom;
+    const margin = { top: 10, right: 20, bottom: 110, left: 40 },
+        width = 650 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
     // Create SVG
     const svg = d3.select("#chart1")
-                  .append("svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                  .attr("transform", `translate(${margin.left},${margin.top})`);
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
     // X Scale
     const x = d3.scaleBand()
-                .domain(songCounts.map(d => d[0]))
-                .range([0, width])
-                .padding(0.1);
+        .domain(songCounts.map(d => d[0]))
+        .range([0, width])
+        .padding(0.1);
 
     // Y Scale
     const y = d3.scaleLinear()
-                .domain([0, d3.max(songCounts, d => d[1])])
-                .nice()
-                .range([height, 0]);
+        .domain([0, d3.max(songCounts, d => d[1])])
+        .nice()
+        .range([height, 0]);
 
     // X Axis
     svg.append("g")
-       .attr("class", "x axis")
-       .attr("transform", `translate(0,${height})`)
-       .call(d3.axisBottom(x))
-       .append("text")
-       .attr("class", "axis-label")
-       .attr("x", width / 2)
-       .attr("y", margin.bottom - 10)
-       .style("text-anchor", "middle")
-       .text("Key");
+        .attr("class", "x axis")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x))
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("x", width / 2)
+        .attr("y", margin.bottom - 10)
+        .style("text-anchor", "middle")
+        .text("Key");
 
     // Y Axis
     svg.append("g")
-       .attr("class", "y axis")
-       .call(d3.axisLeft(y))
-       .append("text")
-       .attr("class", "axis-label")
-       .attr("transform", "rotate(-90)")
-       .attr("y", -margin.left + 10)
-       .attr("x", -height / 2)
-       .attr("dy", "-5.1em")
-       .style("text-anchor", "middle")
-       .text("Number of Songs");
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 10)
+        .attr("x", -height / 2)
+        .attr("dy", "-5.1em")
+        .style("text-anchor", "middle")
+        .text("Number of Songs");
 
     // Tooltip
     const tooltip = d3.select("#tooltip");
 
     // Add bars
     svg.selectAll(".bar")
-       .data(songCounts)
-       .enter().append("rect")
-       .attr("class", "bar")
-       .attr("x", d => x(d[0]))
-       .attr("y", d => y(d[1]))
-       .attr("width", x.bandwidth())
-       .attr("height", d => height - y(d[1]))
-       .on("mouseover", function(event, d) {
-           tooltip.style("opacity", 1);
-           tooltip.html(`Key: ${d[0]}<br>Count: ${d[1]}`)
-                  .style("left", (event.pageX + 5) + "px")
-                  .style("top", (event.pageY - 28) + "px");
-       })
-       .on("mouseout", function() {
-           tooltip.style("opacity", 0);
-       });
-}).catch(function(error) {
+        .data(songCounts)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d[0]))
+        .attr("y", d => y(d[1]))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d[1]))
+        .on("mouseover", function (event, d) {
+            tooltip.style("opacity", 1);
+            tooltip.html(`Key: ${d[0]}<br>Count: ${d[1]}`)
+                .style("left", (event.pageX + 5) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function () {
+            tooltip.style("opacity", 0);
+        });
+}).catch(function (error) {
     console.error('Error loading or parsing data:', error);
 });
 
 // Load data from CSV
-d3.csv("spotify-2023.csv").then(function(data) {
+d3.csv("spotify-2023.csv").then(function (data) {
     // Filter data for the year 2023
     data = data.filter(d => d.released_year === "2023");
 
@@ -99,72 +99,72 @@ d3.csv("spotify-2023.csv").then(function(data) {
 
     // Dimensions
     const margin = { top: 10, right: 20, bottom: 110, left: 40 },
-          width = 800 - margin.left - margin.right,
-          height = 400 - margin.top - margin.bottom;
+        width = 800 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
     // Create SVG
     const svg = d3.select("#chart2")
-                  .append("svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                  .attr("transform", `translate(${margin.left},${margin.top})`);
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
     // X Scale
     const x = d3.scaleBand()
-                .domain(top10Artists.map(d => d[0]))
-                .range([0, width])
-                .padding(0.1);
+        .domain(top10Artists.map(d => d[0]))
+        .range([0, width])
+        .padding(0.1);
 
     // Y Scale
     const y = d3.scaleLinear()
-                .domain([0, d3.max(top10Artists, d => d[1])])
-                .nice()
-                .range([height, 0]);
+        .domain([0, d3.max(top10Artists, d => d[1])])
+        .nice()
+        .range([height, 0]);
 
     // X Axis
     svg.append("g")
-       .attr("class", "x axis")
-       .attr("transform", `translate(0,${height})`)
-       .call(d3.axisBottom(x))
-       .selectAll("text")
-       .attr("transform", "rotate(-45)")
-       .style("text-anchor", "end");
+        .attr("class", "x axis")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "rotate(-45)")
+        .style("text-anchor", "end");
 
     // Y Axis
     svg.append("g")
-       .attr("class", "y axis")
-       .call(d3.axisLeft(y).ticks(10).tickFormat(d3.format("~s")))
-       .append("text")
-       .attr("class", "axis-label")
-       .attr("transform", "rotate(-90)")
-       .attr("y", -margin.left + 10)
-       .attr("x", -height / 2)
-       .attr("dy", "-5.1em")
-       .style("text-anchor", "middle")
-       .text("Number of Streams");
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y).ticks(10).tickFormat(d3.format("~s")))
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 10)
+        .attr("x", -height / 2)
+        .attr("dy", "-5.1em")
+        .style("text-anchor", "middle")
+        .text("Number of Streams");
 
     // Tooltip
     const tooltip = d3.select("#tooltip");
 
     // Add bars
     svg.selectAll(".bar")
-       .data(top10Artists)
-       .enter().append("rect")
-       .attr("class", "bar")
-       .attr("x", d => x(d[0]))
-       .attr("y", d => y(d[1]))
-       .attr("width", x.bandwidth())
-       .attr("height", d => height - y(d[1]))
-       .on("mouseover", function(event, d) {
-           tooltip.style("opacity", 1);
-           tooltip.html(`Artist: ${d[0]}<br>Streams: ${d3.format(",")(d[1])}`)
-                  .style("left", (event.pageX + 5) + "px")
-                  .style("top", (event.pageY - 28) + "px");
-       })
-       .on("mouseout", function() {
-           tooltip.style("opacity", 0);
-       });
-}).catch(function(error) {
+        .data(top10Artists)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d[0]))
+        .attr("y", d => y(d[1]))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d[1]))
+        .on("mouseover", function (event, d) {
+            tooltip.style("opacity", 1);
+            tooltip.html(`Artist: ${d[0]}<br>Streams: ${d3.format(",")(d[1])}`)
+                .style("left", (event.pageX + 5) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function () {
+            tooltip.style("opacity", 0);
+        });
+}).catch(function (error) {
     console.error('Error loading or parsing data:', error);
 });
